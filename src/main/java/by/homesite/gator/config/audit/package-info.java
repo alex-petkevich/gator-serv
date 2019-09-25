1a8b72c48731f71b4c148b7f2cfcf39745a2813d
Aliaksandr Piatkevich <Aliaksandr.Piatkevich@thomsonreuters.com>
2019-09-25 13:24:16 +0300

    Initial application generated by JHipster-6.2.0

---
+++ src/main/java/by/homesite/gator/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package by.homesite.gator.config.audit;
---
+++ src/main/java/by/homesite/gator/web/websocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * WebSocket services, using Spring Websocket.
+ */
+package by.homesite.gator.web.websocket;
