6876423838d6dd27ccceb8e53e179861ed2b76cd
Aliaksandr Piatkevich <Aliaksandr.Piatkevich@thomsonreuters.com>
2020-11-19 19:23:43 +0300

    add build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 chown gator.gator ./target/gator-1.0-beta.jar
 chmod u+x ./target/gator-1.0-beta.jar
-systemctl gator stop
+systemctl stop gator
 sleep 3
 cp ./target/gator-1.0-beta.jar /mnt/vol1/www/gator/gator.jar
-systemctl gator start
+systemctl start gator
