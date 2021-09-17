f79a33a2993cd5d7d6a93f27d949f2790bb18624
alex-petkevich <fktrcfylh>
2021-09-17 17:42:14 +0300

    update settings

--- src/main/java/by/homesite/gator/messaging/NotificationMessageConsumer.java
+++ src/main/java/by/homesite/gator/messaging/NotificationMessageConsumer.java
@@ -1,32 +1,33 @@
 package by.homesite.gator.messaging;
 
-import java.util.List;
-
-import org.springframework.stereotype.Component;
-
 import by.homesite.gator.messaging.dto.Item;
 import by.homesite.gator.service.NotificationService;
 import by.homesite.gator.service.dto.NotificationDTO;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Component
-public class NotificationMessageConsumer
-{
-    private NotificationService notificationService;
+public class NotificationMessageConsumer {
+
+    private final NotificationService notificationService;
+    private static final Logger log = LoggerFactory.getLogger(NotificationMessageConsumer.class);
 
     public NotificationMessageConsumer(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
 
     public void receiveMessage(Item message) {
-        System.out.println("Received <" + message.getId() + ">");
+        log.debug("Received <" + message.getId() + ">");
         List<NotificationDTO> notifications = notificationService.findAll();
-        notifications.forEach(notificationDTO -> {
-            Sender sender = SenderFactory.getSender(notificationDTO.getName());
-            if (sender != null)
-            {
-                sender.sendNotifications(message.getId());
+        notifications.forEach(
+            notificationDTO -> {
+                Sender sender = SenderFactory.getSender(notificationDTO.getName());
+                if (sender != null) {
+                    sender.sendNotifications(message.getId());
+                }
             }
-        });
+        );
     }
-
 }
